93c9584c1978054fc82f543eb0b09a936bc5c0f9
ezeliam <liamdicerbo@gmail.com>
2025-04-16 00:41:12 -0300

    metaetiquetas dinamicas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    // Genera metaetiquetas dinámicas para cada ruta
+    {
+      name: 'html-inject-meta',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          `<meta property="og:image" content="https://liamdicerbo.com/images/preview-portfolio.jpg" />
+          </head>`
+        );
+      }
+    }
+  ],
 })
